8c2797acc68e46405717d66cc1e32bf74f238877
Jai Singh <jaisinghrajput9461@gmail.com>
2025-08-17 15:04:06 +0530

    v 1.0.0

--- BE/src/controllers/admin.controller.js
+++ BE/src/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-import Admin from "../models/Admin.model.js";
+import Admin from "../models/admin.model.js";
 import jwt from "jsonwebtoken";
 import { asyncHandler, ApiError, ApiResponse } from "../utils/index.js";
 
